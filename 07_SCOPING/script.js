36fbf940166bf5c0d07ccd372ad5910b9478efdc
Raja Kumar <rajaraja360parth@gmail.com>
2024-03-04 23:33:08 +0530

    practice of scoping and scope chain in javascript

---
+++ 07_SCOPING/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function calAge(birthYear){
+    const age = 2070 - birthYear;
+    console.log(firstName);
+   
+    function printage(){
+        const output = `your age is ${age} , born in ${birthYear}`;
+        console.log(output);
+    
+if(birthYear>=2000 && birthYear <= 2005){
+    var millenial = true;
+    const firstName = "pratham";
+
+    //output = 'New outpt';
+    const str = `you are a milleneal ${firstName}`;
+    console.log(str);
+
+    function add(a,b){
+        return a+b;
+    }
+}
+
+//add(2+3);
+//console.log(str);
+console.log(millenial);
+}
+    printage();
+    return age;
+}
+
+const firstName = 'Raja';
+const lastName = 'Kumar';
+// console.log(age);
+calAge(2003);
