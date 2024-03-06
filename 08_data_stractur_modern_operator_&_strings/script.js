c08bcb5bb84ccaf55edd2881d9d40680b9a34fd8
Raja Kumar <rajaraja360parth@gmail.com>
2024-03-06 10:41:21 +0530

    rest pattern and parameter practice

--- 08_data_stractur_modern_operator_&_strings/script.js
+++ 08_data_stractur_modern_operator_&_strings/script.js
@@ -65,6 +65,85 @@
 
 //Destracturing Objects:
 
+// const restaurant = {
+//     name: 'Classico Italiano',
+//     location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//     openingHours:{
+//         thu:{
+//             open:12,
+//             close:22,
+//         },
+//         fri:{
+//             open:11,
+//             close:21,
+//         },
+//         sat:{
+//             open:0,
+//             close:24,
+//         }
+//     },
+
+//     order: function(starterIndex,mainIndex){
+//         return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+//     },
+
+//     orderDelivery:function({starterIndex=1,mainIndex=1,time='20:00',address}){
+//         console.log(`order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+//     },
+
+// };
+
+// restaurant.orderDelivery({
+//     time:'20:30',
+//     address:'hossainpr,koldata,107',
+//     mainIndex:2,
+//     starterIndex:3,
+// }); 
+
+
+
+// const {name,openingHours,categories} = restaurant;
+
+// console.log(name,openingHours,categories);
+
+// const{
+//     name:restaurantName,
+//     openingHours:hours,
+//     categories:tags,
+// } = restaurant;
+
+// console.log(restaurantName,hours,tags);
+
+// //setting the default values 
+// const {
+//     menu = [], starterMenu : starters = [],
+// } = restaurant;
+
+// console.log(menu,starters);
+
+// //Mutating variables
+
+// let a = 111;
+// let b = 999;
+// const obj = {a:23,b:4,c:25};
+
+// //{a,b} = obj;  this will show error
+
+// ({a,b} = obj);
+// console.log(a,b);
+
+// //Nested Objects
+// const{fri:{open:o,close:c}} = openingHours;
+// //console.log(fri);
+// console.log(open,close);
+// console.log(o,c);
+
+
+
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -95,49 +174,108 @@ const restaurant = {
         console.log(`order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
     },
 
+    orderPasta:function(ing1,ing2,ing3){
+        console.log(`Here is your delicious pasta with ${ing1},${ing2} and ${ing3}`);
+    },
+
+    orderPizza:function(mainIngredient,...otherIngredient){
+        console.log(mainIngredient);
+        console.log(otherIngredient);
+    }
+
 };
 
-restaurant.orderDelivery({
-    time:'20:30',
-    address:'hossainpr,koldata,107',
-    mainIndex:2,
-    starterIndex:3,
-}); 
+//DESTRUCTURING
+//SPREAD, because on right side of =
+const arr = [1,2,...[4 ,5,6]];
 
+//REST, because on left side of =
+const[a,b,...others] = [1,2,3,4,5];
+console.log(a,b,others);
 
+const[one,,three,...otherFood]=[...restaurant.mainMenu,...restaurant.starterMenu];
 
-const {name,openingHours,categories} = restaurant;
+console.log(one,three,otherFood);
 
-console.log(name,openingHours,categories);
+//REST in objects
 
-const{
-    name:restaurantName,
-    openingHours:hours,
-    categories:tags,
-} = restaurant;
+const{sat,...weekdays} = restaurant.openingHours;
+console.log(weekdays);
 
-console.log(restaurantName,hours,tags);
+//FUNCTIONS
+const add = function(...numbers){
+    console.log(numbers);
+    let sum = 0;
+    for(let i = 0; i<numbers.length; i++){
+        sum+=numbers[i];
+    }
+    console.log(sum);
 
-//setting the default values 
-const {
-    menu = [], starterMenu : starters = [],
-} = restaurant;
+}
+add(2,3);
+add(5,3,7,2);
+add(3,4,5,6,7,2,9,6);
 
-console.log(menu,starters);
+ const x = [4,5,6,7,8];
+ add(...x);
 
-//Mutating variables
+restaurant.orderPizza('mushroom','onion','potato','chili');
+restaurant.orderPizza('only mushroom');
+//SPREAD OPERATOR(...)
+// const arr = [7,8,9];
+// const badArr = [0,1,arr[0],arr[1],arr[2]];
 
-let a = 111;
-let b = 999;
-const obj = {a:23,b:4,c:25};
+// console.log(badArr);
 
-//{a,b} = obj;  this will show error
+// let newArr = [1,2,arr];
+// console.log(newArr);
 
-({a,b} = obj);
-console.log(a,b);
+// newArr = [1,2, ...arr];
+// console.log(newArr);
 
-//Nested Objects
-const{fri:{open:o,close:c}} = openingHours;
-//console.log(fri);
-console.log(open,close);
-console.log(o,c);
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.mainMenu , 'kadhi'];
+
+// console.log(newMenu);
+// console.log(...newMenu);
+
+// //TWO MOST INPORTANT USES OF SPREAD OPERATOR
+
+// //creating a shallow copy
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// //join 2 arrays
+
+// const menu = [...restaurant.starterMenu,...restaurant.mainMenu];
+
+// console.log(menu);
+// console.log(...menu);
+
+// //Iterables arrays,strings,sets, maps but NOT objects
+
+// const str = 'raja';
+// const letters = [...str,' ','k.'];
+// console.log(letters);
+// console.log(...letters);
+// console.log(...str);
+// //console.log(`${str} kumar`);
+
+// const ingredients = 
+// [
+//     // prompt("Let's make pasta! ingredient 1?"),
+//     // prompt("Let's make pasta! ingredient 2?"),
+//     // prompt("Let's make pasta! ingredient 3?")
+// ];
+
+// console.log(ingredients);
+
+// restaurant.orderPasta(ingredients[0],ingredients[1],ingredients[2]);
+
+// restaurant.orderPasta(...ingredients);
+
+// const newRestaurant = {foundIn:1991,...restaurant,founder:'raja',};
+
+// console.log('new restaurant is :',newRestaurant);
+// //console.log(...newRestaurant);
+
