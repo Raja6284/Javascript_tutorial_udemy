243b61653b8c23558f7299ff8d03cb270115918b
Raja Kumar <rajaraja360parth@gmail.com>
2024-03-13 11:54:15 +0530

    done the code of rounding method and  divide and modulo operator

--- 12_Number_dates_timers_Bankist/starter/script.js
+++ 12_Number_dates_timers_Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -251,3 +251,78 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+ //console.log(12 === 12.0);
+
+//Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.33333
+//Binary base 2 - 0 1
+
+// console.log(0.1+0.2);
+// console.log(0.1+0.2 === 0.3);
+
+//conversion
+// console.log(Number('23'));
+// console.log(+'23');
+
+//parsing
+// console.log(Number.parseInt('30px',10));
+// console.log(Number.parseFloat('2.5rem'));
+
+// console.log(Number.parseInt('e23',10));
+
+//check if value is NaN
+
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN(+'20x'));
+// console.log(Number.isNaN(23/0));
+
+//check if value is number
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite(+'20x'));
+// console.log(Number.isFinite(23/0));
+
+// console.log(Number.isInteger(20));
+// console.log(Number.isInteger(20.0));
+// console.log(Number.isInteger('20.0'));
+
+// console.log(Math.max(5,18,11,2));
+// console.log(Math.max(5,'18',11,2));
+// console.log(Math.max(5,'18px',11,2));
+
+// console.log(Math.min(2,3,4,5,35,67,));
+
+// const randomInt =(min,max)=>
+//   Math.trunc(Math.random()*(max-min)+1)+min;
+
+
+// console.log(randomInt(40,50));
+// //Rounding integers
+
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
+
+// console.log(Math.trunc(23.3));
+
+// console.log(Math.trunc(-23.3));
+// console.log(Math.trunc(-23.9));
+
+console.log((2.8).toFixed(0));
+console.log((45.95878945).toFixed(3));
+
+labelBalance.addEventListener('click',function(){
+  [...document.querySelectorAll('.movements__row')].forEach(function(row,i){
+    if(i%2 === 0){
+      row.style.backgroundColor = 'orangered';
+    }
+    if(i%3 === 0){
+      row.style.backgroundColor = 'blue';
+    }
+  });
+});
