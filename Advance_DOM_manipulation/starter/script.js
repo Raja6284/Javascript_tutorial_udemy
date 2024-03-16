0b793a05c0d928d67d550fc79db33c9ea83b9c52
Raja Kumar <rajaraja360parth@gmail.com>
2024-03-16 11:50:35 +0530

    creating the new bankist website under the topic of advance DOM and Events

---
+++ Advance_DOM_manipulation/starter/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn=>btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+//const header = document.querySelector('.header');
+// const allSelections = document.querySelectorAll('.section');
+
+//onsole.log(allSelections);
+
+//Creating and inserting elements
+//.insertAdjacentHTML
+
+//const message = document.createElement('div');
+//message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.<button class = :btn btn--close-cookie"> Got it!</button>';
+
+//message.innerHTML = 'We use cookies for improved functionality and analytics.<button class = "btn btn--close-cookie"> Got it!</button>';
+
+//header.prepend(message);
+//header.append(message);
+//header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+//Delete elements
+//document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+ // message.remove();
+//})
+
+
+//Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+
+// console.log(message.style);
+// console.log(message.style.backgroundColor);
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height = Number.parseFloat(getComputedStyle(message).height,10)+30+'px';
+
